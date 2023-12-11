21167290690a10ca49d325232cba9bc9269ab326
Dmytro Svarkovskiy <100209541+DmytroSvarkovskiy@users.noreply.github.com>
2023-12-11 11:08:45 +0200

    -

--- app/[locale]/advices/page.tsx
+++ app/[locale]/advices/page.tsx
@@ -1,4 +1,3 @@
-'use client';
 import HeroSection from '@/widgets/HeroSection/HeroSection';
 
 import { Metadata } from 'next';
@@ -6,6 +5,7 @@ import { useI18n } from '@/locales/client';
 import dynamic from 'next/dynamic';
 import ArticleList from '@/widgets/ArticleList/ArticleList';
 import AnimalTypes from '@/entities/AnimalTypes/AnimalTypes';
+import { getI18n } from '@/locales/server';
 
 const AdviceFilterBar = dynamic(() => import('@/widgets/AdviceFilterbar/AdviceFilterBar'), {
   ssr: false,
@@ -15,8 +15,8 @@ export const metadata: Metadata = {
   description: 'Top tips for handling pets',
 };
 
-const Advice = () => {
-  const t = useI18n();
+const Advice = async () => {
+  const t = await getI18n();
 
   return (
     <>
